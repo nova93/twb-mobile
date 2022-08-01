353059daf8e1e8e042a18d7b78b0aefa9b0ba382
Robert Moritz <robert@nott-studios.co.uk>
2022-08-01 20:23:07 +0100

    fix next/prev regex

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,11 +1,11 @@
 import { HomeProps } from "../../types/nav";
 
 export default function scraper(data: string): HomeProps {
-  const imageRegex = /<br><img src=".{5,20}"><br>/gim;
+  const imageRegex = /<br><img src=".{4,20}"><br>/gim;
   const justImageRegex = /".*"/;
   const justHTMLRegex = /".*.html/;
-  const prevRegex = /<a href=".{4,15}\.html"><img src="previous\.png"><\/a>/gim;
-  const nextRegex = /<a href=".{4,15}\.html"><img src="next\.png"><\/a>/gim;
+  const prevRegex = /<a href=".{4,20}\.html"><img src="previous\.png"><\/a>/gim;
+  const nextRegex = /<a href=".{4,20}\.html"><img src="next\.png"><\/a>/gim;
 
   const image =
     data.match(imageRegex)?.[0].match(justImageRegex)?.[0].slice(1, -1) ?? null;
